5d250a4688dddf30b4c928fa9531e7de36802d2c
pcherold13 <pcherold13@gmail.com>
2017-05-07 19:40:32 -0500

    Update CacheServer.java

--- Cache/src/CacheServer.java
+++ Cache/src/CacheServer.java
@@ -7,6 +7,8 @@ import java.net.Socket;
  */
 
 public class CacheServer {
+    
+    //Setting up socket and showing acceptance of request
 
     private static int serverPort = 8080;
     private static ServerSocket serverSocket;
@@ -16,15 +18,22 @@ public class CacheServer {
         setupServerSocket();
         while(true){
             try {
+                
+                //Accepting client
+                
                 clientSocket = serverSocket.accept();
                 System.out.println("Accepted.");
             }catch(IOException e){}
 
+            //Start of multithreading and handling of requests
+            
             new Thread(
                     new CacheHandleRequest(clientSocket)
             ).start();
         }
     }
+    
+    //Creating the server socket
 
     public static void setupServerSocket() {
         try {
