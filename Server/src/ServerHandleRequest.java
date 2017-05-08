397ef8699decbd33eb6fbc8858aa25009afd1ceb
pcherold13 <pcherold13@gmail.com>
2017-05-07 19:48:46 -0500

    Update ServerHandleRequest.java

--- Server/src/ServerHandleRequest.java
+++ Server/src/ServerHandleRequest.java
@@ -21,11 +21,15 @@ public class ServerHandleRequest implements Runnable {
     public void run(){
         try{
 
+            //Setting up the request handling
+
             System.out.println("Request received.");
             //TimeUnit.SECONDS.sleep(10);
             long epoch = System.currentTimeMillis() / 1000;
             byte[] inputByteArray = new byte[1000];
             byte[] outputByteArray = new byte[1000];
+
+            //Starting the file streams
 
             InputStream in = clientSocket.getInputStream();
             OutputStream out = clientSocket.getOutputStream();
@@ -34,7 +38,11 @@ public class ServerHandleRequest implements Runnable {
             String data = new String(inputByteArray);
             System.out.println(data);
 
+            //Parsing out the information in the request
+
             String[] lines =  data.split(System.getProperty("line.separator"));
+
+            //Getting the filename
 
             for(String line: lines)
                 if(line.contains("Filename"))
@@ -43,21 +51,30 @@ public class ServerHandleRequest implements Runnable {
             final File file = new File(rootDir, URLDecoder.decode(target, "UTF-8"));
             System.out.println(file.getAbsolutePath());
             if(!file.exists()){
+                
+                //Registering the file doesn't exist and sending such info
+
                 out.write(("HTTP/1.1 404\r\nTime: " + epoch + "\r\n\r\nFile does not exist.").getBytes());
             }else if(!file.canRead()){
-                // something went wrong can't read from file
+                
+                //Something went wrong can't read from file
+                
             }else{
+                
+                //Registering file is on server and sending the file
+
                 FileInputStream fis = new FileInputStream(file);
                 fis.read(outputByteArray);
 
                 out.write(("HTTP/1.1 200\r\nTime: " + epoch + "\r\n\r\n" + new String(outputByteArray)).getBytes());
             }
 
+            //Closing the streams
 
             out.close();
             in.close();
 
-
+            //Outputting completion of request
 
             System.out.println("done");
         }catch(Exception e){
